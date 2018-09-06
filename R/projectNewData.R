c3fbfb633f92b913cd50b2f47599c7c4b265e600
Jose Alquicira Hernandez <alquicirajose@gmail.com>
2018-09-06 21:23:59 +1000

    handles gene expression matrix in genes-by-cells format; uses .intersecLoadings function; returns matrix

--- R/projectNewData.R
+++ R/projectNewData.R
@@ -1,7 +1,7 @@
 #' @title Project new data onto training principal components
 #' @description Projects a new dataset into the principal components obtained from a training dataset
 #' @param object An \code{scPred} object
-#' @param newData A matrix object with cells as rows and genes (loci) as columns
+#' @param newData A matrix object with genes as rows and cells as columns
 #' @param informative Perfoms rotation using only informative principal components
 #' @return A data frame with the projection
 #' @keywords test, validation, projection
@@ -33,25 +33,14 @@ projectNewData <- function(object, newData, informative = TRUE){
     newData <- log2(newData + 1)
   }
   
-  new <- colnames(newData)
-  ref <- rownames(getLoadings(object))
   
-  
-  if(!(all(new %in% ref) & all(ref %in% new))){ # Subset genes if necesary
-    newSub <- newData[,new %in% ref] 
-    refSub <- getLoadings(object)[ref %in% new, ]
-    newSub <- newSub[, match(rownames(refSub), colnames(newSub))]
-  }else if(!all(new == ref)){ # Order new data according to loadings matrix
-    newSub <- newData[, match(ref, new)]
-    refSub <- getLoadings(object)
-  }else{ # Use data directly if genes match and are ordered
-    newSub <- newData
-    refSub <- getLoadings(object)
-  }
-  
-  # Get new centers and scales for gene features
-  newCenter <- object@pca$center[names(object@pca$center) %in% colnames(newSub)]
-  newScale <- object@pca$scale[names(object@pca$scale) %in% colnames(newSub)]
+  res <- .intersectLoadings(ref = getLoadings(object), new =  newData)
+  refSub <- res$ref
+  newSub <- res$new
+  rm(res)
+
+  newCenter <- object@svd$center[names(object@svd$center) %in% rownames(newSub)]
+  newScale <- object@svd$scale[names(object@svd$scale) %in% rownames(newSub)]
   
   if(informative){
   informativePCs  <- object@features %>% 
@@ -61,14 +50,14 @@ projectNewData <- function(object, newData, informative = TRUE){
       unique() 
   
   features <- colnames(refSub) %in% informativePCs
-  refSub <- refSub[,features]
+  refSub <- refSub[,features, drop = FALSE]
   }
   
   
   
   # Perform linear transformation
-  newDataProj <- scale(newSub, newCenter, newScale) %*% refSub
+  newDataProj <- scale(t(newSub), newCenter, newScale) %*% refSub
   
-  as.data.frame(newDataProj)
+  newDataProj
   
 }
