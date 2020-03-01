4ee790f92af8b1025ca92cfa6e4e699d40bbc85d
joseah <alquicirajose@gmail.com>
2020-03-01 22:05:56 +1100

    adapts scPred object

--- R/trainModel.R
+++ R/trainModel.R
@@ -1,42 +1,40 @@
 #' @title Train a prediction model
-#' @description Trains a prediction model from an \code{scPred} object
-#' @param object An \code{scPred} object with informative PCs obtained using
-#' the \code{getInformativePCs} function
+#' @description Trains a prediction model from an \code{scPred} object stored in a \code{Seurat} object
+#' @param object An \code{Seurat} object with informative PCs obtained using
+#' the \code{getFeatureSpace} function
 #' @param model Classification model supported via \code{caret} package. A list of all models can be found here:
 #' https://topepo.github.io/caret/available-models.html
 #' Default: support vector machine with polynomial kernel
-#' @param resampleMethod Resample model used in \code{trainControl} function. Default: K-fold cross validation
+#' @param resampleMethod Resample model used in \code{trainControl} function from \code{caret}. 
+#' Default: K-fold cross validation
 #' @param number Number of iterations for resample method. See \code{trainControl} function
-#' @param seed Numeric seed for resample method
+#' @param seed Numeric seed for resample method. Fixed to ensure reproducibility
 #' @param metric Performance metric to be used to select best model: `ROC` (area under the ROC curve), 
 #' `PR` (area under the precision-recall curve), `Accuracy`, and `Kappa`
-#' @param imbalance Proportion of cell type compositions to be considered as an imbalance issue. By default,
-#' if a cell type is only present in 0.1 (10%) or less from the whole population, scPred will attempt to 
+#' @param imbalance Proportion of cell type composition to be considered as an imbalance issue. By default,
+#' if a cell type is present only in 0.1 (10%) or less from the whole population, scPred will attempt to 
 #' train a weighted SVM to account for class imbalance. Set to `1` to avoid this step
-#' @param returnData If \code{TRUE}, training data is returned
-#' @param savePredictions an indicator of how much of the hold-out predictions for each resample should be
-#' saved. Values can be either "all", "final", or "none". A logical value can also be used that convert to
-#'  "all" (for true) or "none" (for false). "final" saves the predictions for the optimal tuning parameters.
-#' @return A list of  \code{train} objects for each cell class (e.g. cell type). See \code{train} function for details.
+#' @param returnData If \code{TRUE}, training data is returned within \code{scPred} object. 
+#' @param savePredictions Specifies the set of hold-out predictions for each resample that should be
+#' returned. Values can be either "all", "final", or "none".
+#' @return A list of \code{train} objects for each cell class (e.g. cell type). See \code{train} function for details.
 #' @keywords train, model
 #' @importFrom methods is
 #' @importFrom pbapply pblapply
 #' @export
 #' @author
-#' José Alquicira Hernández
+#' Jose Alquicira Hernandez
 #' @examples
 #'
 #' # Train a SVM with a Radial kernel
 #' ## A numeric seed is provided for the K-fold cross validation
 #' ## The metric ROC is used to select the best tuned model. "Accuracy" and "Kappa" may be used too.
 #'
-#' object <- trainModel(object = object,
+#' pbmc_small <- getFeatureSpace(pbmc_small, "RNA_snn_res.0.8")
+#' pbmc_small <- trainModel(object = pbmc_small,
 #'                      seed = 1234,
 #'                      metric = "ROC")
 #'
-
-
-
 
 trainModel <- function(object,
                        model = "svmRadial",
@@ -53,38 +51,15 @@ trainModel <- function(object,
     # Validations -------------------------------------------------------------
     
     # Check class
-    if(!any(is(object, "scPred") | is(object, "Seurat"))){
-        stop("object must be 'scPred' or 'Seurat'")
+    if(!is(object, "Seurat")){
+        stop("object must be 'Seurat'")
     }
     
-    if(is(object, "scPred")){
-        
-        # Check metadata
-        if(nrow(object@metadata) == 0){
-            stop("No metadata has been assigned to object")
-        }
-        
-        # Validate if features have been determined
-        if(length(object@features) == 0){
-            stop("No features have been determined. Use 'getFeatureSpace()' function")
-        }
-        
-        
-        classes <- names(object@features)
-        
-        if(is.null(classes)){
-            stop("Prediction variable is not contained in metadata")
-        }
-        
-    }else{
-        
-        if(is.null(object@misc$scPred)){
-            stop("No features have been determined. Use 'getFeatureSpace()' function")
-        }
-        classes <- names(object@misc$scPred$features)
-        
+    if(is.null(object@misc$scPred)){
+        stop("No features have been determined. Use 'getFeatureSpace()' function")
     }
     
+    classes <- names(object@misc$scPred@features)
     metric <- match.arg(metric)
     
     
@@ -121,11 +96,8 @@ trainModel <- function(object,
         names(modelsRes) <- classes
     }
     
-    if(inherits(object, "scPred")){
-        object@train <- modelsRes
-    }else{
-        object@misc$scPred$train <- modelsRes
-    }
+    
+    object@misc$scPred@train <- modelsRes
     object
 }
 
@@ -142,34 +114,16 @@ trainModel <- function(object,
                         allowParallel){
     
     
-    if(is(object, "scPred")){
-        
-        if(nrow(object@features[[positiveClass]]) == 0){
-            message("No informative principal components were identified for class: ", positiveClass)
-        }
-        
-        namesPC <- as.character(object@features[[positiveClass]]$PC)
-        features <- subsetMatrix(getPCA(object), namesPC)
-        
-        
-        # Get and refactor response variable according to positive class
-        # According to twoClassSummary() documentation
-        ## "If assumes that the first level of the factor variables corresponds to a relevant result
-        ## but the lev argument can be used to change this."
-        response <-  as.character(object@metadata[[object@pVar]])
-
-        
-    }else{
-        
-        if(nrow(object@misc$scPred$features[[positiveClass]]) == 0){
-            message("No informative principal components were identified for class: ", positiveClass)
-        }
-        
-        namesPC <- as.character(object@misc$scPred$features[[positiveClass]]$PC)
-        features <- subsetMatrix(Embeddings(object), namesPC)
-        response <-  as.character(object[[object@misc$scPred$pVar, drop = TRUE]])
-        
+    
+    
+    if(nrow(object@misc$scPred@features[[positiveClass]]) == 0){
+        message("No informative principal components were identified for class: ", positiveClass)
     }
+    
+    namesPC <- as.character(object@misc$scPred@features[[positiveClass]]$PC)
+    features <- subsetMatrix(Embeddings(object, reduction = "pca"), namesPC)
+    response <-  as.character(object[[object@misc$scPred@pVar, drop = TRUE]])
+    
     
     classWeight <- table(response)/ length(response)
     
@@ -215,14 +169,14 @@ trainModel <- function(object,
     
     
     if(metric == "AUC"){
-    fit <- train(x = features,
-                 y = response,
-                 method = model,
-                 metric = metric,
-                 trControl = trCtrl,
-                 tuneGrid = expand.grid(sigma = .05,
-                                        C = c(.25, .5, 1),
-                                        Weight = 1:2))
+        fit <- train(x = features,
+                     y = response,
+                     method = model,
+                     metric = metric,
+                     trControl = trCtrl,
+                     tuneGrid = expand.grid(sigma = .05,
+                                            C = c(.25, .5, 1),
+                                            Weight = 1:2))
     }else{
         fit <- train(x = features,
                      y = response,
