af5492e778b076e529c20462c92aacd06c75bdc0
joseah <alquicirajose@gmail.com>
2021-07-13 18:46:23 +1000

    modularizes data projection/alignment in `project_query` function

---
+++ R/project_query.R
@@ -0,0 +1,163 @@
+#' @title Project query data onto PCA reference
+#' @description Projects data from Seurat object onto a pre-computed PCA
+#' @param new A Seurat object containing cells to be projected
+#' @param reference A \code{Seurat} object after running \code{getFeatureSpace()}
+#' @param max.iter.harmony Maximum number of rounds to run Harmony. One round of Harmony involves one clustering and one correction step.
+#' @param recompute_alignment Recompute alignment? Useful if \code{scPredict()} has already been run
+#' @param seed Numeric seed for harmony 
+#' @param ... Further arguments to \code{HarmonyMatrix()}
+#' @return A Seurat object with two new reductions: 
+#' \itemize{
+#' \item \code{scpred}: Aligned data using harmony
+#' \item \code{scpred_projection}: Raw projection using reference loadings
+#' }
+#' @keywords prediction, new, test, validation
+#' @importFrom methods is
+#' @importFrom SeuratObject Embeddings Stdev DefaultAssay CreateDimReducObject VariableFeatures Loadings GetAssayData
+#' @importFrom harmony HarmonyMatrix
+#' @export
+#' @author
+#' José Alquicira Hernández
+
+
+project_query <- function(new, 
+                          reference, 
+                          max.iter.harmony = 20,
+                          recompute_alignment = TRUE,
+                          seed = 66, 
+                          ...){
+
+
+# Validate if provided object is an scPred object
+if(!(is(reference, "Seurat") | is(reference, "scPred"))) stop("'object' must be of class 'scPred' or 'Seurat'")
+
+if(is(reference, "Seurat")){
+  spmodel <- reference@misc$scPred
+}else{
+  spmodel <- reference
+}
+
+if(is.null(spmodel)) stop("No feature space has been determined!")
+if(!is(new, "Seurat")) stop("New data must be a Seurat object")
+
+
+# Dataset alignment -------------------------------------------------------
+
+if("scpred" %in% names(new@reductions)){
+  if(recompute_alignment){
+    alignment <- TRUE
+    cat(crayon::yellow(cli::symbol$figure_dash, "Data has already been aligned to a reference.\n"), sep = "")
+    cat(crayon::yellow(cli::symbol$sup_plus, "Skip data alignment using `recompute.alignment = FALSE`.\n"),  sep = "")
+      
+  } 
+  else {
+    alignment <- FALSE
+  }
+  
+}else{
+  alignment <- TRUE
+}
+
+if(alignment){
+  
+  cat(crayon::green(cli::symbol$record, " Matching reference with new dataset...\n"))
+  
+  # Subset data
+  ref_loadings <- spmodel@feature_loadings
+  ref_embeddings <- spmodel@cell_embeddings
+  new_features <- rownames(new)
+  
+  # Get genes
+  reference_features <- rownames(ref_loadings)
+  
+  
+  # Get intersection between reference and new datasets
+  shared_features <- intersect(reference_features, new_features)
+  cat(crayon::cyan("\t", cli::symbol$line, paste(length(reference_features), "features present in reference loadings\n")))
+  cat(crayon::cyan("\t", cli::symbol$line, paste(length(shared_features), "features shared between reference and new dataset\n")))
+  cat(crayon::cyan("\t", cli::symbol$line, paste0(round(length(shared_features)/length(reference_features) * 100, 2), 
+                                                  "% of features in the reference are present in new dataset\n")))
+  
+  
+  # Subset shared genes from reference
+  ref_loadings <- ref_loadings[shared_features, ]
+  
+  
+  
+  new_data <- GetAssayData(new, "data")[shared_features,]
+  means <- spmodel@scaling$means
+  stdevs  <- spmodel@scaling$stdevs
+  new_data <- Matrix::t(new_data)
+  names(means) <- rownames(spmodel@scaling) -> names(stdevs)
+  
+  # Subset means and standard deviations
+  means <- means[shared_features]
+  stdevs <- stdevs[shared_features]
+  #all(colnames(new_data) == names(means))
+  
+  i <- stdevs == 0
+  
+  if(any(i)){
+    warning(paste0(sum(i), " features have zero variance but are present in the feature loadings. \nDid you subset or integrated this data before?"))
+    cat(crayon::yellow("Removing zero-variance genes from projection\n"))
+    
+    new_data <- new_data[,!i]
+    ref_loadings <- ref_loadings[!i,]
+    means <- means[!i]
+    stdevs <- stdevs[!i]
+    #all(colnames(new_data) == rownames(ref_loadings))
+  }
+  
+  
+  scaled_data <- scale(new_data, means, stdevs)
+  
+  
+  new_embeddings <- scaled_data %*% ref_loadings
+  
+  
+  
+  dataset <- factor(c(rep("reference", nrow(ref_embeddings)), rep("new", nrow(new_embeddings))), 
+                    levels = c("reference", "new"))
+  
+  
+  rownames(ref_embeddings) <- paste0("ref_", rownames(ref_embeddings))
+  rownames(new_embeddings) <- paste0("new_", rownames(new_embeddings))
+  
+  
+  eigenspace <- as.data.frame(rbind(ref_embeddings, new_embeddings))
+  meta_data <- data.frame(rownames(eigenspace), dataset = dataset)
+  
+  cat(crayon::green(cli::symbol$record, " Aligning new data to reference...\n"))
+  
+  set.seed(seed)
+  harmony_embeddings <- HarmonyMatrix(eigenspace, 
+                                      meta_data, 
+                                      'dataset', 
+                                      do_pca = FALSE, 
+                                      reference_values = "reference",
+                                      max.iter.harmony = max.iter.harmony, 
+                                      ...)
+  
+  new_embeddings_aligned <- harmony_embeddings[dataset == "new", , drop = FALSE]
+  
+}else{
+  new_embeddings_aligned <- Embeddings(new, reduction = "scpred")
+  colnames(new_embeddings_aligned) <- gsub("scpred_", spmodel@reduction_key, colnames(new_embeddings_aligned))
+}
+
+  
+  rownames(new_embeddings_aligned) <- gsub("^new_", "", rownames(new_embeddings_aligned))
+  new@reductions[["scpred"]] <- CreateDimReducObject(embeddings = new_embeddings_aligned, 
+                                                     key = "scpred_",
+                                                     assay = DefaultAssay(object = new))
+  if(recompute_alignment){
+    
+    rownames(new_embeddings) <- gsub("^new_", "", rownames(new_embeddings))
+    new@reductions[["scpred_projection"]] <- CreateDimReducObject(embeddings = new_embeddings, 
+                                                                  key = "Projection_",
+                                                                  assay = DefaultAssay(object = new))
+  }
+
+  new
+  
+}
--- R/scPredict.R
+++ R/scPredict.R
@@ -1,7 +1,7 @@
 #' @title Predict cell classes from a new dataset using a trained model
 #' @description Predicts cell classes for a new dataset based on trained model(s)
 #' @param new A seurat object containing cells to be classified
-#' @param reference An \code{Seurat} object with trained model(s) using \code{scPred} or an \code{scPred} object
+#' @param reference A \code{Seurat} object with trained model(s) using \code{scPred} or an \code{scPred} object
 #' @param threshold Threshold used for probabilities to classify cells into classes. All cells below 
 #' this threshold value will be labels as "unassigned". In the case of binary classification (two cell tyoes),
 #' a threshold of \code{0.5} will force all cells to be classified to any of the two cell types. For multi-class
@@ -10,14 +10,18 @@
 #' @param max.iter.harmony Maximum number of rounds to run Harmony. One round of Harmony involves one clustering and one correction step.
 #' @param recompute_alignment Recompute alignment? Useful if \code{scPredict()} has already been run
 #' @param seed Numeric seed for harmony 
-#' @return A Seurat object with addtional metadata columns with prediction probabilities associated to each class, a \code{prediction} column, 
-#' indicating the classification based on the provided threshold and a \code{generic_class} column without "unassigned" labels.
+#' @return A Seurat object with additional metadata columns with prediction probabilities associated to each class, a \code{prediction} column, 
+#' indicating the classification based on the provided threshold and a \code{generic_class} column without "unassigned" labels. Additionally,
+#' two new reductions are returned: 
+#' \itemize{
+#' \item \code{scpred}: Aligned data using harmony
+#' \item \code{scpred_projection}: Raw projection using reference loadings
+#' }
 #' @keywords prediction, new, test, validation
 #' @importFrom methods is
 #' @importFrom stats predict
-#' @importFrom SeuratObject Embeddings Stdev AddMetaData DefaultAssay CreateDimReducObject VariableFeatures Loadings GetAssayData
+#' @importFrom SeuratObject Embeddings AddMetaData
 #' @importFrom pbapply pblapply
-#' @importFrom harmony HarmonyMatrix
 #' @export
 #' @author
 #' José Alquicira Hernández
@@ -45,111 +49,17 @@ scPredict <- function(new,
   if(!length(spmodel@train)) stop("No models have been trained!")
   if(!is(new, "Seurat")) stop("New data must be a Seurat object")
   
-  
-  
-  # Dataset alignment -------------------------------------------------------
-  
-  if("scpred" %in% names(new@reductions)){
-    if(recompute_alignment){
-      alignment <- TRUE
-      crayon::yellow(cli::symbol$figure_dash, "Data has already been aligned to a reference.\n") %>% 
-        cat(sep = "")
-      crayon::yellow(cli::symbol$sup_plus, "Skip data alignment using `recompute.alignment = FALSE`.\n") %>% 
-        cat(sep = "")
-    } 
-    else {
-      alignment <- FALSE
-    }
-    
-  }else{
-    alignment <- TRUE
-  }
-  
-  if(alignment){
-    
-    cat(crayon::green(cli::symbol$record, " Matching reference with new dataset...\n"))
-    
-    # Subset data
-    ref_loadings <- spmodel@feature_loadings
-    ref_embeddings <- spmodel@cell_embeddings
-    new_features <- rownames(new)
-    
-    # Get genes
-    reference_features <- rownames(ref_loadings)
-    
-    
-    # Get intersection between reference and new datasets
-    shared_features <- intersect(reference_features, new_features)
-    cat(crayon::cyan("\t", cli::symbol$line, paste(length(reference_features), "features present in reference loadings\n")))
-    cat(crayon::cyan("\t", cli::symbol$line, paste(length(shared_features), "features shared between reference and new dataset\n")))
-    cat(crayon::cyan("\t", cli::symbol$line, paste0(round(length(shared_features)/length(reference_features) * 100, 2), 
-                                                    "% of features in the reference are present in new dataset\n")))
-    
-    
-    # Subset shared genes from reference
-    ref_loadings <- ref_loadings[shared_features, ]
-    
-    
-    
-      new_data <- GetAssayData(new, "data")[shared_features,]
-      means <- spmodel@scaling$means
-      stdevs  <- spmodel@scaling$stdevs
-      new_data <- Matrix::t(new_data)
-      names(means) <- rownames(spmodel@scaling) -> names(stdevs)
-      
-      # Subset means and standard deviations
-      means <- means[shared_features]
-      stdevs <- stdevs[shared_features]
-      #all(colnames(new_data) == names(means))
 
-      i <- stdevs == 0
-      
-      if(any(i)){
-        warning(paste0(sum(i), " features have zero variance but are present in the feature loadings. \nDid you subset or integrated this data before?"))
-        cat(crayon::yellow("Removing zero-variance genes from projection\n"))
-        
-        new_data <- new_data[,!i]
-        ref_loadings <- ref_loadings[!i,]
-        means <- means[!i]
-        stdevs <- stdevs[!i]
-        #all(colnames(new_data) == rownames(ref_loadings))
-      }
-      
-      
-      scaled_data <- scale(new_data, means, stdevs)
-      
-      
-      new_embeddings <- scaled_data %*% ref_loadings
-      
-    
-    
-    dataset <- factor(c(rep("reference", nrow(ref_embeddings)), rep("new", nrow(new_embeddings))), 
-                      levels = c("reference", "new"))
-    
-    
-    rownames(ref_embeddings) <- paste0("ref_", rownames(ref_embeddings))
-    rownames(new_embeddings) <- paste0("new_", rownames(new_embeddings))
-    
-    
-    eigenspace <- as.data.frame(rbind(ref_embeddings, new_embeddings))
-    meta_data <- data.frame(rownames(eigenspace), dataset = dataset)
-    
-    cat(crayon::green(cli::symbol$record, " Aligning new data to reference...\n"))
-    
-    set.seed(seed)
-    harmony_embeddings <- HarmonyMatrix(eigenspace, 
-                                        meta_data, 
-                                        'dataset', 
-                                        do_pca = FALSE, 
-                                        reference_values = "reference",
-                                        max.iter.harmony = max.iter.harmony)
-    
-    new_embeddings_aligned <- harmony_embeddings[dataset == "new", , drop = FALSE]
-    
-  }else{
-    new_embeddings_aligned <- Embeddings(new, reduction = "scpred")
-    colnames(new_embeddings_aligned) <- gsub("scpred_", spmodel@reduction_key, colnames(new_embeddings_aligned))
-  }
+  # Project query data ------------------------------------------------------
+
+  new <- project_query(new, 
+                reference = spmodel, 
+                max.iter.harmony = max.iter.harmony, 
+                recompute_alignment = recompute_alignment,
+                seed = seed)
+  
+  new_embeddings_aligned <- Embeddings(new[["scpred"]])
+  colnames(new_embeddings_aligned) <- colnames(spmodel@cell_embeddings)
   
   
   
@@ -181,7 +91,7 @@ scPredict <- function(new,
   res <- sapply(cellTypeModelNames, .predictCellClass, spmodel, new_embeddings_aligned)
   
   # Gather results
-  res <- as.data.frame(Reduce(cbind, res))
+  res <- as.data.frame(res)
   colnames(res) <- cellTypeModelNames
   rownames(res) <- colnames(new)
   
@@ -223,17 +133,6 @@ scPredict <- function(new,
   
   # Return results
   new <- AddMetaData(new, res)
-  rownames(new_embeddings_aligned) <- gsub("^new_", "", rownames(new_embeddings_aligned))
-  new@reductions[["scpred"]] <- CreateDimReducObject(embeddings = new_embeddings_aligned, 
-                                                     key = "scpred_",
-                                                     assay = DefaultAssay(object = new))
-  if(recompute_alignment){
-    
-    rownames(new_embeddings) <- gsub("^new_", "", rownames(new_embeddings))
-    new@reductions[["scpred_projection"]] <- CreateDimReducObject(embeddings = new_embeddings, 
-                                                           key = "Projection_",
-                                                           assay = DefaultAssay(object = new))
-  }
   
   cat(crayon::green("DONE!\n"))
   
